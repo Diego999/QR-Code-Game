b990485649eb397edfa575629170e21d711ecdd9
Diego Antognini <diegoantognini@gmail.com>
2015-01-25 17:38:53 +0100

    Add BFS

--- qr_code_game_BFS.py
+++ qr_code_game_BFS.py
@@ -1,5 +1,4 @@
 from Queue import Queue
-from sets import Set
 
 class Vertex:
 
@@ -27,7 +26,13 @@ class Vertex:
 		self._p = p
 
 	def _to_string(self):
-		return str(self._id)
+		output = str(self._id) + ' parent : ' 
+		if self._p == Vertex.NO_PARENT:
+			output += '- '
+		else:
+			output += str(self._p.get_id())
+
+		return output + ', distance : ' + str(self._d)
 
 	def __hash__(self):
 		return self._id
@@ -58,7 +63,7 @@ class Edge:
 		return self._w
 
 	def _to_string(self):
-		return str(self._u) + ' -' + str(self._w) + '-> ' + str(self._v)
+		return str(self._u.get_id()) + ' =' + str(self._w) + '= > ' + str(self._v.get_id())
 
 	def __str__(self):
 		return self._to_string()
@@ -81,6 +86,28 @@ class SimpleGraph:
 	def add_edge(self, u, v, w):
 		self._edges[u].append(Edge(u, v, w))
 
+	def breadth_first_search(self, s, queue_extra_content=[]):
+		for v in self._vertices:
+			v.set_p(Vertex.NO_PARENT)
+			v.set_d(Vertex.NO_DISTANCE)
+
+		q = Queue()
+		q.put(s)
+		s.set_d(0)
+		for v in queue_extra_content:
+			q.put(v)
+		
+		tree = []
+		while not q.empty():
+			u = q.get()
+			for e in self._edges[u]:
+				v = e.get_v()
+				if v.get_d() == Vertex.NO_DISTANCE:
+					v.set_d(u.get_d() + e.get_w())
+					v.set_p(u)
+					tree.append(e)
+					q.put(v)
+		return tree
 
 	def _to_string(self):
 		s = ''
@@ -102,3 +129,24 @@ class SimpleGraph:
 if __name__ == "__main__":
 	g = SimpleGraph()
 
+	v = []
+	for i in range(0, 9):
+		v.append(g.add_vertex(i))
+
+	g.add_edge(v[0], v[1], 22)
+	g.add_edge(v[0], v[3], 11)
+	g.add_edge(v[2], v[1], 55)
+	g.add_edge(v[4], v[2], 44)
+	g.add_edge(v[1], v[4], 33)
+	g.add_edge(v[3], v[4], 88)
+	g.add_edge(v[5], v[3], 66)
+
+	g.add_edge(v[3], v[6], 77)
+	g.add_edge(v[6], v[5], 99)
+	g.add_edge(v[6], v[8], 1010)
+	g.add_edge(v[6], v[7], 1212)
+	g.add_edge(v[7], v[6], 1111)
+	g.add_edge(v[8], v[7], 13)
+	
+
+	g.breadth_first_search(v[0])
