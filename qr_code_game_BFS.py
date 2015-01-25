b21d273fdec3c8115be871808603555d28ce1583
Diego Antognini <diegoantognini@gmail.com>
2015-01-25 17:55:01 +0100

    Prepare the representation for the BFS solver

--- qr_code_game_BFS.py
+++ qr_code_game_BFS.py
@@ -1,4 +1,8 @@
+from pngtools import PNGTools
+from qrcodegenerator import QRCodeGenerator
 from Queue import Queue
+
+from graphsolver import GraphSolver
 
 class Vertex:
 
@@ -109,10 +113,16 @@ class SimpleGraph:
 					q.put(v)
 		return tree
 
+	def get_vertices(self):
+		return self._vertices
+
+	def get_edges(self):
+		return self._edges
+
 	def _to_string(self):
 		s = ''
 		for v in self._vertices:
-			s += str(v) + ' -> '
+			s += str(v.get_id()) + ' -> '
 			if v in self._edges:
 				for e in self._edges[v]:
 					s += str(e) + ' |'
@@ -126,27 +136,76 @@ class SimpleGraph:
 	def __repr__(self):
 		return self._to_string()
 
+class SimpleGraphSolver:
+
+	EMPTY_WEIGHT_EDGE = 0
+	FULL_WEIGHT_EDGE = 1
+
+	def __init__(self, message, filename):
+		self._qr_code_generate = QRCodeGenerator()
+		self._png_tools = PNGTools()
+		self._filename = filename
+		self._matrix = self._qr_code_generate.generate(message, filename)
+		self._g = SimpleGraph()
+
+		self._vertices = [] # #vertices = W*W
+		self._edges = [] # #edges = W*(W-1)*2*2
+		self._start_vertices = [] # #vertices = W
+		self._exit_vertices = [] # #vertices = W
+
+	# O(V^2)
+	def init(self):
+		self._generate_vertices()
+		self._generate_edges()
+		self._find_start_end_vertices()
+
+		print self._g
+
+	# O(V^2)
+	def _generate_vertices(self):
+		for i in range(0, len(self._matrix)):
+			self._vertices.append([])
+			for j in range(0, len(self._matrix[i])):
+				self._vertices[-1].append(self._g.add_vertex(i*len(self._matrix) + j))
+		
+	# O(W*(W-1)*2*2) => O(W^2) => O(V)
+	def _generate_edges(self):
+		for i in range(0,len(self._matrix[0])):
+			self._edges.append([])
+			for j in range(0, len(self._matrix[i])):
+				self._edges[-1].append([])
+				self._compute_add_value_edge(i, j, SimpleGraphSolver.EMPTY_WEIGHT_EDGE, SimpleGraphSolver.FULL_WEIGHT_EDGE)
+
+	# O(2*W) => O(V^1/2)
+	def _find_start_end_vertices(self):
+		for j in range(0, len(self._matrix[0])):
+			self._start_vertices.append(self._vertices[0][j])
+			self._exit_vertices.append(self._vertices[-1][j])
+
+	# O(4) => O(1)
+	def _compute_add_value_edge(self, i, j, value_empty, value_full):
+		tuples = [(-1,0),(1,0),(0,-1),(0,1)]
+		neighboors = []
+		for t in tuples:
+			if i+t[0] >= 0 and i+t[0] < len(self._matrix[0]) and j+t[1] >= 0 and j+t[1] < len(self._matrix):
+				neighboors.append((i+t[0], j+t[1]))
+
+		current_value = self._matrix[i][j]
+		for n in neighboors:
+			neighboor_value = self._matrix[n[0]][n[1]]
+
+			#Define the case where the weight of an edge should be EMPTY or FULL
+			if neighboor_value == GraphSolver.WHITE and current_value == GraphSolver.WHITE or neighboor_value == GraphSolver.WHITE and current_value == GraphSolver.BLACK:
+				value = value_empty 
+			else:
+				value = value_full
+
+			e = self._g.add_edge(self._vertices[i][j], self._vertices[n[0]][n[1]], value)
+			self._edges[-1][-1].append(e)
+
 if __name__ == "__main__":
-	g = SimpleGraph()
+	message = 'D'#In matters of truth and justice, there is no difference between large and small problems, for issues concerning the treatment of people are all the same. Albert Einstein'
+	filename_qr_code = 'code.png'
 
-	v = []
-	for i in range(0, 9):
-		v.append(g.add_vertex(i))
-
-	g.add_edge(v[0], v[1], 22)
-	g.add_edge(v[0], v[3], 11)
-	g.add_edge(v[2], v[1], 55)
-	g.add_edge(v[4], v[2], 44)
-	g.add_edge(v[1], v[4], 33)
-	g.add_edge(v[3], v[4], 88)
-	g.add_edge(v[5], v[3], 66)
-
-	g.add_edge(v[3], v[6], 77)
-	g.add_edge(v[6], v[5], 99)
-	g.add_edge(v[6], v[8], 1010)
-	g.add_edge(v[6], v[7], 1212)
-	g.add_edge(v[7], v[6], 1111)
-	g.add_edge(v[8], v[7], 13)
-	
-
-	g.breadth_first_search(v[0])
+	solver = SimpleGraphSolver(message, filename_qr_code)
+	solver.init()
